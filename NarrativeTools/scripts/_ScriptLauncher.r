e95095942e7cadbcdc258de0e592fb66c210a559
Christoper Tracey <ctracey@paconserve.org>
2020-12-21 09:20:49 -0500

    PABS changes because my program doenst communicate

--- NarrativeTools/scripts/_ScriptLauncher.r
+++ NarrativeTools/scripts/_ScriptLauncher.r
@@ -10,10 +10,10 @@ rm(list = ls())
 source(here::here("scripts", "0_PathsAndSettings.r"))
 ################################################################
 # Enter the name of your NHA into the script to run step 2 and 3
-LauncherNHA <- "Little Mahoning Creek Tributary"
+LauncherNHA <- "McIntire Run Slope"
 
 # Run the Template Databaser
 source(here::here("scripts","2_NHA_TemplateReader.r"))
 # Run the pdf Maker
 source(here::here("scripts","3_Generate_NHA_PDF.r"))
-☻
+
