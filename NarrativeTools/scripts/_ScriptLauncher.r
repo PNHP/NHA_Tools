eda4bf1988f68481642254ba8111dd17c57fcd53
Christoper Tracey <ctracey@paconserve.org>
2020-12-29 20:52:57 -0500

    sig para regional sens

--- NarrativeTools/scripts/_ScriptLauncher.r
+++ NarrativeTools/scripts/_ScriptLauncher.r
@@ -3,7 +3,7 @@ rm(list=ls()) # clear the environment
 source(here::here("scripts", "0_PathsAndSettings.r")) # load in the paths and settings file
 ################################################################
 # Enter the name of your NHA into the script to run step 2 and 3
-LauncherNHA <- "Lashell's Hollow"
+LauncherNHA <- "Davistown"
 FinalSwitch <- "Final" # "Draft"
 # Run the Template Databaser
 source(here::here("scripts","2_NHA_TemplateReader.r"))
@@ -13,5 +13,3 @@ source(here::here("scripts","3_Generate_NHA_PDF.r"))
 
 
 
-
-Rice's Landing
