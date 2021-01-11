59401a3cda2cf993e842f72b8b441a2c49787614
Christoper Tracey <ctracey@paconserve.org>
2021-01-10 20:58:08 -0500

    looper improvements

--- NarrativeTools/scripts/_ScriptLauncher.r
+++ NarrativeTools/scripts/_ScriptLauncher.r
@@ -3,7 +3,7 @@ rm(list=ls()) # clear the environment
 source(here::here("scripts", "0_PathsAndSettings.r")) # load in the paths and settings file
 ################################################################
 # Enter the name of your NHA into the script to run step 2 and 3
-LauncherNHA <- "Beaver River near Fallston"
+LauncherNHA <- "Sumney Road"
 FinalSwitch <- "Final" # "Draft"
 # Run the Template Databaser
 source(here::here("scripts","2_NHA_TemplateReader.r"))
@@ -12,17 +12,24 @@ source(here::here("scripts","3_Generate_NHA_PDF.r"))
 
 
 
-
-
-
-
-Brady's Run County Park
-Brush Creek at Glen Eden Road
-Brush Creek at the Pennsylvania Turnpike
-Brush Creek Park
-Brush Run
-
-
+## LOOP VERSION 
+t <- read.csv(here::here("scripts","looplist.csv"), stringsAsFactors=FALSE)
+names(t) <- "SiteName"
+t <- unique(t)
+for(q in 1:nrow(t)){
+  print(t$SiteName[q])
+  #
+  rm(list=setdiff(ls(), c("t","q"))) # clear the environment
+  source(here::here("scripts", "0_PathsAndSettings.r")) # load in the paths and settings file
+  ################################################################
+  # Enter the name of your NHA into the script to run step 2 and 3
+  LauncherNHA <- t$SiteName[q]
+  FinalSwitch <- "Final" # "Draft"
+  # Run the Template Databaser
+  source(here::here("scripts","2_NHA_TemplateReader.r"))
+  # Run the pdf Maker
+  source(here::here("scripts","3_Generate_NHA_PDF.r"))
+}
 
 
 
