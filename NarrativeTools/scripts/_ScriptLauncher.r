7e65e1068b45a5060585769e03c1297ec1925064
Christoper Tracey <ctracey@paconserve.org>
2021-01-13 11:03:45 -0500

    catchup

--- NarrativeTools/scripts/_ScriptLauncher.r
+++ NarrativeTools/scripts/_ScriptLauncher.r
@@ -3,7 +3,7 @@ rm(list=ls()) # clear the environment
 source(here::here("scripts", "0_PathsAndSettings.r")) # load in the paths and settings file
 ################################################################
 # Enter the name of your NHA into the script to run step 2 and 3
-LauncherNHA <- "Marianna"
+LauncherNHA <- "State Game Land #153"
 FinalSwitch <- "Final" # "Draft"
 # Run the Template Databaser
 source(here::here("scripts","2_NHA_TemplateReader.r"))
@@ -13,7 +13,7 @@ source(here::here("scripts","3_Generate_NHA_PDF.r"))
 
 ################################################################
 ## LOOP VERSION 
-t <- read.csv(here::here("scripts","looplist_b.csv"), stringsAsFactors=FALSE)
+t <- read.csv(here::here("scripts","looplist.csv"), stringsAsFactors=FALSE)
 names(t) <- "SiteName"
 t <- unique(t)
 for(q in 1:nrow(t)){
@@ -31,5 +31,6 @@ for(q in 1:nrow(t)){
   source(here::here("scripts","3_Generate_NHA_PDF.r"))
 }
 ################################################################
+
 
 
