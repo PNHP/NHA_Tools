d6b0147809cb44360272f6ddb06ca7a0b60389b9
Christopher Tracey <ctracey@paconserve.org>
2022-04-26 14:19:56 -0400

    fixed paths

--- SpatialTools/NHI_ExportToPNG_NHA.py
+++ SpatialTools/NHI_ExportToPNG_NHA.py
@@ -12,8 +12,8 @@
 import arcpy, os, sys
 
 # set paths to aprx and output folder
-aprx = r"E:\\NHA_CountyIntroMaps\\NHA_CountyIntroMaps_NHA.aprx"
-output_folder = r"P:\\Conservation Programs\\Natural Heritage Program\\ConservationPlanning\\NaturalHeritageAreas\\_NHA\\z_BaseImages\\introMaps"
+aprx = r"W:\\Heritage\\Conservation_Planning\\NaturalHeritageAreas\\_MapTemplates\\NHA_SiteAccountMap\\NHA_SiteAccountMap.aprx"
+output_folder = r"W:\\Heritage\\Conservation_Planning\\NaturalHeritageAreas\\_MapTemplates\\NHA_SiteAccountMap"
 
 # set projects, layouts, and establish map series
 p = arcpy.mp.ArcGISProject(aprx)
@@ -24,6 +24,6 @@ if not l.mapSeries is None:
 if ms.enabled:
     for pageNum in range(1, ms.pageCount + 1):
         ms.currentPageNumber = pageNum
-        print("Exporting {0}".format(ms.pageRow.COUNTY_NAM))
-        pageName = ms.pageRow.COUNTY_NAM
-        l.exportToPNG(os.path.join(output_folder,"Layout_NHA_" + ''.join(e for e in (ms.pageRow.COUNTY_NAM) if e.isalnum()) + ".png"),resolution=200)
+        print("Exporting {0}".format(ms.pageRow.SITE_NAME))
+        pageName = ms.pageRow.SITE_NAME
+        l.exportToPNG(os.path.join(output_folder,"Layout_NHA_" + ''.join(e for e in (ms.pageRow.SITE_NAME) if e.isalnum()) + ".png"),resolution=200)
